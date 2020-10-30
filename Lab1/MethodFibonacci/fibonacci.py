9247c3c47b088d814f54794fad5258bbf86131c3
JelliedFish <hungrydog757@gmail.com>
2020-10-31 00:28:35 +0300

    Add the Fibonacci Method

--- Lab1/MethodFibonacci/fibonacci.py
+++ Lab1/MethodFibonacci/fibonacci.py
@@ -3,41 +3,37 @@ import math
 from Lab1.Functions.functions import f1, f2, f3, f4, f5
 
 
-def F(n):
-    return (1 / math.sqrt(5)) * (((1 + math.sqrt(5)) / 2) ** n - ((1 - math.sqrt(5)) / 2) ** n)
+def fibonachi(n):
+    if n in (1, 2):
+        return 1
+    return fibonachi(n - 1) + fibonachi(n - 2)
 
 
 def minimization(f, a, b):
-    delta = 0.00001
+    n = 20
 
-    n = 1
-
-    while F(n) <= (b - a) / delta:
-        n += 1
-
-    x1 = a + (F(n - 2) / F(n)) * (b - a)
-    x2 = a + (F(n - 1) / F(n)) * (b - a)
-    f1 = f(x1)
-    f2 = f(x2)
-
-    for k in range(2, n - 2):
-
-        if f1 <= f2:
-            b = x2
-            x2 = x1
-            x1 = a + (F(n - k) / F(n - k + 1)) * (b - a)
-            f1 = f(x1)
-
+    def findDot(a, b, n, k, is_first):
+        if is_first:
+            return a + (fibonachi(n - k - 1) * (b - a)) / fibonachi(n - k + 1)
         else:
+            return a + (fibonachi(n - k) * (b - a)) / fibonachi(n - k + 1)
+
+    x1 = findDot(a, b, n, 1, True)
+    x2 = findDot(a, b, n, 1, False)
+
+    for k in range(2, n + 1):
+
+        if f(x1) > f(x2):
             a = x1
             x1 = x2
-            x2 = a + (F(n - k) / F(n - k + 1)) * (b - a)
-            f2 = f(x2)
-
-    min = (x1 + x2) / 2
-    minf = f(min)
-
-    print(minf)
+            x2 = findDot(a, b, n, k, False)
+        else:
+            b = x2
+            x2 = x1
+            x1 = findDot(a, b, n, k, True)
+        if k == n - 2:
+            break
+    print(f((x1 + x2) / 2))
 
 
 minimization(f1, -0.5, 0.5)
